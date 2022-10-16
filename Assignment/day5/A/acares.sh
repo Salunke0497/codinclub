9533a8b87d9ce3598e403abe42ccbe5072cd2fd5
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-10-16 20:25:55 +0530

    Update acares.sh

--- Assignment/day5/A/acares.sh
+++ Assignment/day5/A/acares.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash -x
+x=60;
+y=40;
+z=25;
+l=$(($x*305/1000))
+w=$(($y*305/1000))
+A=$(($l*$w))
+echo "total area in square feet"
+echo $A
+ans=$(($z*$A))
+echo "total area in meter square"
+echo $ans
