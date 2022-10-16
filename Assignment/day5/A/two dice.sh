6f22ce6a38efee3b58ddebab39d47af976ae1f77
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-10-16 21:47:01 +0530

    Create two dice.sh

---
+++ Assignment/day5/A/two dice.sh
@@ -0,0 +1,42 @@
+x=$((RANDOM%6))
+case $x in
+        1)
+        echo "1"
+        ;;
+        2)
+        echo "2"
+        ;;
+        3)
+        echo "3"
+        ;;
+        4)
+        echo "4"
+        ;;
+        5)
+        echo "5"
+        ;;
+        *)
+        echo "6"
+        ;;
+esac
+y=$((RANDOM%6))
+case $y in
+        1)
+        echo "1"
+        ;;
+        2)
+        echo "2"
+        ;;
+        3)
+        echo "3"
+        ;;
+        4)
+        echo "4"
+        ;;
+        5)
+        echo "5"
+        ;;
+        *)
+        echo "6"
+        ;;
+esac
