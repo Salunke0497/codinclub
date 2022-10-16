3be18b3824b363edc359e957eb15212ac50f5bb9
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-10-16 13:36:31 +0530

    Delete Assignment directory

--- Assignment/DICEadd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-echo "dice 1 number between 1-6"
-echo "dice 2 number between 1-6"
-DICE1=$(( RANDOM%6 ))
-DICE2=$(( RANDOM%6 ))
-TOTAL=$((  $DICE1 + $DICE2))
-echo $TOTAL
--- Assignment/dice.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-echo "dice number between 1-6"
-echo=$(( RANDOM%6 ))
-echo $echo
