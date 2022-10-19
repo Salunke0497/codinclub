8292de14dfd21d668e519278dd85dec0390c4dfd
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-10-19 23:58:50 +0530

    Create n0. to week day.sh

---
+++ Assignment/day5/B/if elif else/n0. to week day.sh
@@ -0,0 +1,33 @@
+echo "the number is"
+read -p "" x
+if
+        ((x==1))
+then
+        echo "monday"
+elif
+        ((x==2))
+then
+        echo "tuesday"
+elif
+        ((x==3))
+then
+        echo "wenesday"
+elif
+        ((x==4))
+then
+        echo "thirsday"
+elif
+        ((x==5))
+then
+        echo "friday"
+elif
+        ((x==6))
+then
+        echo "saturday"
+elif
+        ((x==7))
+then
+        echo "sunday"
+else
+        echo "only 7 days present into the weak"
+fi
