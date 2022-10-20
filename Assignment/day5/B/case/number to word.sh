82a0ebb507b8c44aef4a2e1e053229a3f611d514
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-10-20 16:26:15 +0530

    Create number to word.sh

---
+++ Assignment/day5/B/case/number to word.sh
@@ -0,0 +1,28 @@
+echo "enter the number here"
+read -p "" x
+case $x in
+        1)
+        echo "one"
+        ;;
+        2)
+        echo "two"
+        ;;
+        3)
+        echo "three"
+        ;;
+        4)
+        echo "four"
+        ;;
+        5)
+        echo "five"
+        ;;
+        6)
+        echo "seven"
+        ;;
+        8)
+        echo "eight"
+        ;;
+        *)
+        echo "nine"
+        ;;
+esac
