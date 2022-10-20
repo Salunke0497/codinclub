e98e2421614d2596938ad2461f644760cb006e92
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2022-10-20 17:05:52 +0530

    Create num to word.sh

---
+++ Assignment/day5/B/case/num to word.sh
@@ -0,0 +1,25 @@
+echo "enter the number here"
+read -p "" x
+case $x in
+        1)
+        echo "sunday"
+        ;;
+        2)
+        echo "monday"
+        ;;
+        3)
+        echo "tuesday"
+        ;;
+        4)
+        echo "wenusday"
+        ;;
+        5)
+        echo "thirsday"
+        ;;
+        6)
+        echo "friday"
+        ;;
+        *)
+        echo "saturday"
+        ;;
+esac
